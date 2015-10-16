7d58fc75a79440e6f0ccc776345577cb8a25d40d
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-10-16 12:12:50 -0400

    Add some ideas about monads comprehension building blocks.

--- docs/source/monads.rst
+++ docs/source/monads.rst
@@ -1,5 +1,97 @@
-_monads:
+.. _monads:
 
 ======================
  Monads Comprehension
 ======================
+
+.. math::
+   :nowrap:
+
+   \begin{eqnarray}
+      {\bf foldr}^\tau & :: & (\alpha \rightarrow \beta \rightarrow \beta)
+          \rightarrow \beta \rightarrow \tau \alpha \rightarrow \beta \\
+      {\bf foldr}^\tau (\oplus) z []^\tau & = & z \\
+      {\bf foldr}^\tau (\oplus)\ z\ (x :^\tau xs) & = & x \oplus
+         ({\bf foldr}^\tau (\oplus)\ z\ xs)
+   \end{eqnarray}
+
+
+
+Internal representation
+=======================
+
+.. module:: xotl.ql._monads
+
+.. autoclass:: Empty()
+
+.. autoclass:: Cons(x, xs)
+
+   This is an *abstract* representation of the "x : xs" operation as referred
+   in [QLFunc]_.  It's not meant to be efficient or to be used as true
+   collection for Python programs.  Furthermore
+
+   It serves the purpose of expressing queries and it will form the basic
+   building block for the `query object`:term:.
+
+   The `xs` must be a *collection* or another `Cons` object.
+
+   Instances support the extraction of head and tail we see in functional
+   languages::
+
+     >>> head, tail = Cons(1, [2, 3])
+     >>> head
+     1
+
+     >>> tail
+     Cons(2, Cons(3, Empty()))
+
+   Any of the arguments may take the value be `xoutil.Undefined`:obj: to
+   "partialize" the constructor.  Using this feature you may declare the
+   monadic Unit operator as::
+
+     >>> from xoutil import Undefined
+     >>> Unit = Cons(Undefined, [])
+
+   Then building the "unit" can be done::
+
+     >>> Unit(1)
+     Cons(1, Empty())
+
+   There's no direct support for walking the `Cons` besides its ability to
+   extract the head and tail.  Walking is easily defined by recursion or
+   iteration::
+
+     >>> def last(c):
+     ...    h, t = c
+     ...    return h if not t else last(t)
+
+     >>> last(Cons(1, [2, 3]))
+     3
+
+     >>> def last2(c):
+     ...    h, t = c
+     ...    while t:
+     ...       h, t = t
+     ...    return h
+
+     >>> last2(Cons(1, [2, 3]))
+     3
+
+   Internally the head is the attribute `x` and the tail, attribute `xs`.
+
+.. autoclass:: Foldr(f, z, l)
+
+   The `foldr` operation is a generalization of the `reduce` function.  It
+   operates as illustrated below::
+
+                       +
+                      / \
+                     x1  +
+                        / \
+                      x2   z
+
+   As noted in [QLFunc]_ the `Cons`:class: operator (:math:`x :^\tau xs`)
+   needs to be further specified for *set* and *bags*.
+
+   However, for the purposes of `xotl.ql` this is only meant for description
+   and not functionality.
---
+++ xotl/ql/_monads.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# _monads
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-10-15
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from xoutil import Undefined
+from xoutil.eight.meta import metaclass
+
+
+class EmptyClass(type):
+    def __instancecheck__(cls, instance):
+        if instance is Undefined:
+            return True
+        else:
+            from xoutil.types import is_collection
+            return is_collection(instance) and not bool(instance)
+
+
+class Empty(metaclass(EmptyClass)):
+    '''Any empty collection.
+
+    As a special case Undefined is considered an empty collection.
+
+    '''
+    def __repr__(self):
+        return 'Empty()'
+
+    def __unicode__(self):
+        return '∅'.decode('utf-8')
+
+    def __bool__(self):
+        return False
+    __nonzero__ = __bool__
+
+
+class Cons(object):
+    r'''The collection constructor "x : xs".
+
+
+    '''
+    @staticmethod
+    def _head(collection):
+        def _inner():
+            i = iter(collection)
+            yield next(i)
+            try:
+                peek = next(i)
+            except StopIteration:
+                yield Empty()
+            else:
+                from itertools import chain
+                yield chain((peek, ), (x for x in i))
+        if not isinstance(collection, Empty):
+            return _inner()
+        else:
+            raise ValueError('Cannot extract the head of an empty collection.')
+
+    def __init__(self, *args):
+        Cons = type(self)
+        x, xs = Undefined, Undefined
+        if args:
+            x, args = args[0], args[1:]
+        if args:
+            xs, args = args[0], args[1:]
+        assert not args
+        self.x = x
+        if xs and not isinstance(xs, Cons):
+            self.xs = Cons(*tuple(Cons._head(xs)))
+        elif xs is not Undefined and not xs:
+            self.xs = Empty()
+        else:
+            self.xs = xs
+
+    def __bool__(self):
+        return bool(self.x)
+    __nonzero__ = __bool__
+
+    def __call__(self, *args):
+        Cons = type(self)
+        x, xs = self.x, self.xs
+        if x is not Undefined and xs is not Undefined:
+            raise TypeError('Fully qualified Cons')
+        if x is Undefined and args:
+            x = args[0]
+            args = args[1:]
+        if xs is Undefined and args:
+            xs = args[0]
+            args = args[1:]
+        assert not args
+        return Cons(x, xs)
+
+    def __iter__(self):
+        def _iter():
+            yield self.x
+            yield self.xs
+        if self.x is not Undefined:
+            return _iter()
+        else:
+            raise TypeError('Cons as a partial function cannot be iterated')
+
+    def __repr__(self):
+        if self.xs is not Undefined:
+            return 'Cons(%r, %r)' % (self.x, self.xs)
+        else:
+            return 'Cons(%r)' % self.x
+
+
+# Quote:
+#
+#   Thus, in order for `foldr + z` to be well-defined, `+` has to be
+#   commutative or idempotent whenever (:) is left-commutative or
+#   left-idempotent respectively.  This ensures the meaning of `foldr + z` to
+#   be independent of the actual construction of its argument.
+#
+#   -- [QLFunc]_
+
+class Foldr(object):
+    '''The structural recursion operator.
+
+    `foldr` is defined by::
+
+       foldr                ::  (a -> B -> B) -> B -> T a -> B
+       foldr + z []         =   z
+       foldr + z (x : xs)   =   x + (foldr + z xs)
+
+    Where `[]` is the empty collection that makes sense for the type `T`, and
+    the `+` is any operator that make sense for the items in the collections
+    (type `a` in the formulae.)  The `x : xs` is the `Cons`:class:.
+
+    '''
+
+    def __init__(self, *args):
+        operator, arg, collection, args = self._parse_args(args)
+        assert not args
+        self.operator = operator
+        self.arg = arg
+        self.collection = collection
+
+    def __call__(self, *args):
+        Foldr = type(self)
+        operator, arg, collection = self._get_args(args)
+        if Undefined in (operator, arg, collection):
+            return Foldr(operator, arg, collection)
+        if isinstance(collection, Empty):
+            return arg
+        else:
+            x, xs = collection
+            return operator(x, Foldr(operator, arg, xs)())
+
+    def __iter__(self):
+        # This makes `Join(Cons(Cons(1, []), Undefined))` to fail.  But since
+        # Join requires a single argument we will have to make a Undefined a
+        # `state` and not a value.  So `instance(Cons(1, Undefined),
+        # Undefined)` be True and all checks about definiteness would have to
+        # change.
+        #
+        # Let's see if we need that.
+        #
+        # yield self
+        # yield Empty()
+        pass
+
+    def _get_args(self, args):
+        operator = self.operator
+        arg = self.arg
+        collection = self.collection
+        if operator is Undefined and args:
+            operator, args = args[0], args[1:]
+        if arg is Undefined and args:
+            arg, args = args[0], args[1:]
+        if collection is Undefined and args:
+            collection, args = args[0], args[1:]
+        assert not args
+        return operator, arg, collection
+
+    @classmethod
+    def _parse_args(cls, args):
+        if args:
+            operator, args = args[0], args[1:]
+        else:
+            operator = Undefined
+        if args:
+            z, args = args[0], args[1:]
+        else:
+            z = Undefined
+        if args:
+            l, args = args[0], args[1:]
+        else:
+            l = Undefined
+        return (operator, z, l, args)
+
+
+class Union(object):
+    # Unions are resolved as soon possible by Cons.
+    def __new__(cls, xs=Undefined, ys=Undefined):
+        res = super(Union, cls).__new__(cls)
+        res.__init__(xs, ys)
+        return res()
+
+    def __init__(self, xs=Undefined, ys=Undefined):
+        self.xs = xs
+        self.ys = ys
+
+    def __call__(self, *args):
+        if self.xs is Undefined and args:
+            xs, args = args[0], args[1:]
+        else:
+            xs = self.xs
+        if self.ys is Undefined and args:
+            ys, args = args[0], args[1:]
+        else:
+            ys = self.ys
+        assert not args
+        if xs is Undefined or ys is Undefined:
+            if xs is self.xs and ys is self.ys:
+                return self  # stop recursion in __new__
+            else:
+                return Union(xs, ys)
+        elif isinstance(xs, Empty):
+            return ys
+        else:
+            x, xs = xs
+            return Cons(x, Union(xs, ys))
+
+    def __iter__(self):
+        raise TypeError('Partial union is not iterable')
+
+
+
+# Monadic contructors
+Zero = Empty
+Unit = Cons(Undefined, Empty())
+
+
+class _Mapper(object):
+    # Simply wraps the function of a map, so that the original function is not
+    # just a closure-accessible value, but exposed to the query translators.
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, x, xs):
+        return Cons(self.f(x), xs)
+
+
+Map = lambda f: Foldr(_Mapper(f), Empty())
+Join = Foldr(Union, Empty())
+
+# Translation from comprehension syntax to monadic constructors
+#
+# MC [e | ]       ≝ Unit(MC e)
+# MC [e | x <- q] ≝ map (λx. MC e) (MC q)
+# MC [e | p ]     ≝ if MC p then MC e else Zero()
+# MC [e | q, p]   ≝ join(MC [MC [e | p] | q])
+# MC e            ≝ e   # other cases
