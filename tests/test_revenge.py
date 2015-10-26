5215fe8c2d42809132eced82e7bc2c65a9591f68
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-10-26 11:26:06 -0400

    Add our basic test suite for revenge.

--- tests/test_revenge.py
+++ tests/test_revenge.py
@@ -16,22 +16,44 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
+import pytest
+import sys
+_py3 = sys.version_info >= (3, 0)
+del sys
 
-def test_basic_uncompyler():
+
+@pytest.mark.xfail(_py3, reason='Still not running on Py3k')
+def test_ifexpression():
+    from xotl.ql.revenge import Uncompyled
+    l = lambda x, y: x if x else y
+    u = Uncompyled(l)
+    assert u.source == 'if x:\n    return x\nreturn y'
+
+
+def test_lambda():
+    from xotl.ql.revenge import Uncompyled
+    l = lambda: lambda x, y, a=1, *args, **kwargs: x + y + a
+    u = Uncompyled(l)
+    assert u.source == 'return lambda x, y, a=1, *args, **kwargs: x + y + a'
+
+    u = Uncompyled(l())
+    assert u.source == 'return x + y + a'
+
+
+def test_expressions():
     from xotl.ql.revenge import Uncompyled
 
-    def fib(n):
-        a = b = 1
-        while a < n:
-            yield a
-            a, b = b, a + b
-
-    def fibl(n):
-        return list(a for a in fib(n))
-
-    u = Uncompyled(fib)
-    assert u.source == ('a = b = 1\nwhile a < n:\n    '
-                        'yield a\n    a, b = b, a + b')
-
-    u = Uncompyled(fibl)
-    assert u.source == 'return list((a for a in fib(n)))'
+    expressions = [
+        'a + b',
+        '[a for a in x if a < y]',
+        '(a for a in this if a < y)',
+        '{k: v for k, v in this}',
+        '{s for s in this if s < y}'
+    ]
+    codes = [(compile(expr, '<test>', 'eval'), expr) for expr in expressions]
+    failures = []
+    for code, expr in codes:
+        u = Uncompyled(code)
+        if u.source != 'return ' + expr:
+            failures.append((expr, u.source))
+    assert not failures
