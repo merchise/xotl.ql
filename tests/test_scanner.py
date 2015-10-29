4e5bd3fedead43d57427897f88e49453f444d77e
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-10-29 17:17:33 -0400

    Add another scanner tools test.

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -84,9 +84,9 @@ def test_scanner_normalization():
 
 def test_pypy_normalization():
     from xotl.ql.revenge.scanners import Instruction, LOAD_NAME
-    from xotl.ql.revenge.scanners import POP_JUMP_IF_FALSE, NOP
+    from xotl.ql.revenge.scanners import POP_JUMP_IF_FALSE, POP_JUMP_IF_TRUE
     from xotl.ql.revenge.scanners import RETURN_VALUE, JUMP_FORWARD
-    from xotl.ql.revenge.scanners import JUMP_ABSOLUTE
+    from xotl.ql.revenge.scanners import JUMP_ABSOLUTE, NOP
     from xotl.ql.revenge.scanners import normalize_pypy_conditional
 
     # The PyPy byte code for `a if b else c`:
@@ -161,6 +161,60 @@ def test_pypy_normalization():
 
         Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
                     arg=2, argval='y', argrepr='y', offset=12,
+                    starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    res = list(normalize_pypy_conditional(pypy_program))
+    assert modified_pypy_program == res
+
+    # ``a if not b else c``
+    pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='b', argrepr='b',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_TRUE', opcode=POP_JUMP_IF_TRUE,
+                    arg=12, argval=12, argrepr='',
+                    offset=3, starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=1, argval='a', argrepr='a',
+                    offset=6, starts_line=None, is_jump_target=False),
+        Instruction(opname='JUMP_FORWARD', opcode=JUMP_FORWARD,
+                    arg=3, argval=15, argrepr='to 15',
+                    offset=9, starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='c', argrepr='c',
+                    offset=12, starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    modified_pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='b', argrepr='b',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_TRUE', opcode=POP_JUMP_IF_TRUE,
+                    arg=12, argval=12, argrepr='', offset=3,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME, arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE, arg=None,
+                    argval=None, argrepr='', offset=9,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=10,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=11,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='c', argrepr='c', offset=12,
                     starts_line=None, is_jump_target=True),
         Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
                     arg=None, argval=None, argrepr='',
