b1cdbca8e7f3012d53d64ae6e3ebffd54a28c767
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-11-09 18:18:32 -0500

    Add Python specific sphinx extensions.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@ import datetime
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(1, os.path.abspath('./ext/'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -40,7 +41,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.todo',
               'sphinx.ext.mathjax',
               'sphinx.ext.intersphinx',
-              'sphinx.ext.ifconfig',]
+              'sphinx.ext.ifconfig',
+              'pyspec', ]
 
 
 if '/home/manu' in __file__:
