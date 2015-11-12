1fe633eeef5b3c991d6b249056066bced6264fbf
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-11-11 22:48:26 -0500

    Join all revenge tests.

--- tests/test_revenge.py
+++ tests/test_revenge.py
@@ -21,6 +21,240 @@ _py3 = sys.version_info >= (3, 0)
 del sys
 
 
+def test_scanner_normalization():
+    from xotl.ql.revenge.scanners import InstructionSetBuilder
+    from xotl.ql.revenge.scanners import without_nops
+
+    # The PyPy byte code for `a if b else c`:
+    builder = InstructionSetBuilder()
+    with builder() as Instruction:
+        Instruction(opname='LOAD_NAME',
+                    arg=0, argval='x', argrepr='x',
+                    starts_line=1),
+        Instruction(opname='POP_JUMP_IF_FALSE',
+                    arg=12, argval=12, argrepr='',
+                    starts_line=None),
+        Instruction(opname='LOAD_NAME', arg=1,
+                    argval='a', argrepr='a',
+                    starts_line=None),
+        # Replace the JUMP_FORWARD
+        # Instruction(opname='JUMP_FORWARD', opcode=JUMP_FORWARD,
+        #             arg=3, argval=15, argrepr='to 15', offset=9,
+        #             starts_line=None, is_jump_target=False),
+        Instruction(opname='RETURN_VALUE', arg=None,
+                    argval=None, argrepr='',
+                    starts_line=None),
+        Instruction(opname='NOP', arg=None,
+                    argval=None, argrepr='',
+                    starts_line=None),
+        Instruction(opname='NOP', arg=None,
+                    argval=None, argrepr='',
+                    starts_line=None),
+        # End of replacement
+        Instruction(opname='LOAD_NAME',
+                    arg=2, argval='y', argrepr='y',
+                    starts_line=None),
+        Instruction(opname='RETURN_VALUE',
+                    arg=None, argval=None, argrepr='',
+                    starts_line=None)
+    modified_pypy_program = list(builder)
+
+    builder = InstructionSetBuilder()
+    with builder() as Instruction:
+        Instruction(opname='LOAD_NAME', arg=0,
+                    argval='x', argrepr='x',
+                    starts_line=1),
+        Instruction(opname='POP_JUMP_IF_FALSE',
+                    arg=10, argval=10, argrepr='',
+                    starts_line=None),
+        Instruction(opname='LOAD_NAME', arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None),
+        Instruction(opname='RETURN_VALUE',
+                    arg=None, argval=None, argrepr='',
+                    starts_line=None),
+        Instruction(opname='LOAD_NAME', arg=2,
+                    argval='y', argrepr='y',
+                    starts_line=None),
+        Instruction(opname='RETURN_VALUE', arg=None,
+                    argval=None, argrepr='', starts_line=None)
+    expected = list(builder)
+    res = list(without_nops(modified_pypy_program))
+    assert res == expected
+
+
+def test_pypy_normalization():
+    from xotl.ql.revenge.scanners import Instruction, LOAD_NAME
+    from xotl.ql.revenge.scanners import POP_JUMP_IF_FALSE, POP_JUMP_IF_TRUE
+    from xotl.ql.revenge.scanners import RETURN_VALUE, JUMP_FORWARD
+    from xotl.ql.revenge.scanners import JUMP_ABSOLUTE, NOP
+    from xotl.ql.revenge.scanners import normalize_pypy_conditional
+
+    # The PyPy byte code for `a if b else c`:
+    pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='x', argrepr='x',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_FALSE', opcode=POP_JUMP_IF_FALSE,
+                    arg=12, argval=12, argrepr='', offset=3,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME, arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='JUMP_FORWARD', opcode=JUMP_FORWARD,
+                    arg=3, argval=15, argrepr='to 15', offset=9,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='y', argrepr='y', offset=12,
+                    starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    modified_pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='x', argrepr='x',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_FALSE', opcode=POP_JUMP_IF_FALSE,
+                    arg=12, argval=12, argrepr='', offset=3,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME, arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE, arg=None,
+                    argval=None, argrepr='', offset=9,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=10,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=11,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='y', argrepr='y', offset=12,
+                    starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    res = list(normalize_pypy_conditional(pypy_program))
+    assert modified_pypy_program == res
+
+    # Same program with JUMP_ABSOLUTE
+    pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='x', argrepr='x',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_FALSE', opcode=POP_JUMP_IF_FALSE,
+                    arg=12, argval=12, argrepr='', offset=3,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME, arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='JUMP_ABSOLUTE', opcode=JUMP_ABSOLUTE,
+                    arg=15, argval=15, argrepr='to 15', offset=9,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='y', argrepr='y', offset=12,
+                    starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    res = list(normalize_pypy_conditional(pypy_program))
+    assert modified_pypy_program == res
+
+    # ``a if not b else c``
+    pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='b', argrepr='b',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_TRUE', opcode=POP_JUMP_IF_TRUE,
+                    arg=12, argval=12, argrepr='',
+                    offset=3, starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=1, argval='a', argrepr='a',
+                    offset=6, starts_line=None, is_jump_target=False),
+        Instruction(opname='JUMP_FORWARD', opcode=JUMP_FORWARD,
+                    arg=3, argval=15, argrepr='to 15',
+                    offset=9, starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='c', argrepr='c',
+                    offset=12, starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    modified_pypy_program = [
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=0, argval='b', argrepr='b',
+                    offset=0, starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_TRUE', opcode=POP_JUMP_IF_TRUE,
+                    arg=12, argval=12, argrepr='', offset=3,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME, arg=1,
+                    argval='a', argrepr='a', offset=6,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE, arg=None,
+                    argval=None, argrepr='', offset=9,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=10,
+                    starts_line=None, is_jump_target=False),
+        Instruction(opname='NOP', opcode=NOP, arg=None,
+                    argval=None, argrepr='', offset=11,
+                    starts_line=None, is_jump_target=False),
+
+        Instruction(opname='LOAD_NAME', opcode=LOAD_NAME,
+                    arg=2, argval='c', argrepr='c', offset=12,
+                    starts_line=None, is_jump_target=True),
+        Instruction(opname='RETURN_VALUE', opcode=RETURN_VALUE,
+                    arg=None, argval=None, argrepr='',
+                    offset=15, starts_line=None, is_jump_target=True)
+    ]
+
+    res = list(normalize_pypy_conditional(pypy_program))
+    assert modified_pypy_program == res
+
+
+def test_real_pypy_normalization():
+    from xotl.ql.revenge.scanners import InstructionSetBuilder, label
+    from xotl.ql.revenge.scanners import getscanner
+
+    # The PyPy byte code for `a if x else y`:
+    builder = InstructionSetBuilder()
+    with builder() as Instruction:
+        Instruction(opname='LOAD_NAME',
+                    arg=0, argval='x', argrepr='x',
+                    starts_line=1, is_jump_target=False),
+        Instruction(opname='POP_JUMP_IF_FALSE',
+                    arg=label('else y'), starts_line=None)
+        Instruction(opname='LOAD_NAME', arg=1,
+                    argval='a', argrepr='a', starts_line=None)
+        Instruction(opname='RETURN_VALUE',
+                    arg=None, argval=None, argrepr='', starts_line=None)
+        Instruction(label='else y',
+                    opname='LOAD_NAME',
+                    arg=2, argval='y', argrepr='y', starts_line=None),
+        Instruction(opname='RETURN_VALUE',
+                    arg=None, argval=None, argrepr='',
+                    starts_line=None)
+    expected_program = list(builder)
+    scanner = getscanner()
+    tokens, customize = scanner.disassemble(compile('a if x else y', '', 'eval'))
+    instructions = [token.instruction for token in tokens if token.instruction]
+    assert instructions == expected_program
+
+
 # We're only testing we can build an AST from the byte-code.  This AST
 # extracted from the byte-code directly and not the one we'll provide to
 # translators.  The idea is to stabilize the parser from byte-code to this IST
@@ -50,9 +284,12 @@ def test_conditional_expressions():
     expressions = [
         # expr, expected source if different
         ('a if x else y', None),
+        ('a and (b or c)', None),
         ('a and b or c', None),
         ('(a if x else y) if (b if z else c) else (d if o else p)', None),
+        ('(a if x else y) if (b if z else c) else (d if not o else p)', None),
         ('(a if x else y) if not (b if not z else c) else (d if o else p)', None),
+        ('(a if not x else y) if not (b if not z else c) else (d if not o else p)', None),
         ('c(a if x else y)', None),
         ('lambda : (a if x else y)', None),
         ('(lambda: x) if x else (lambda y: y)(y)', None),
@@ -60,9 +297,50 @@ def test_conditional_expressions():
     _do_test(expressions)
 
 
+def test_conditional_a_la_pypy():
+    # >>> dis.dis(compile('x and a or y', '', 'eval'))
+    #   1           0 LOAD_NAME                0 (x)
+    #               3 JUMP_IF_FALSE_OR_POP     9
+    #               6 LOAD_NAME                1 (a)
+    #         >>    9 JUMP_IF_TRUE_OR_POP     15
+    #              12 LOAD_NAME                2 (y)
+    #         >>   15 RETURN_VALUE
+    import types
+    from xotl.ql.revenge import Uncompyled
+    from xotl.ql.revenge.scanners import InstructionSetBuilder, label
+    builder = InstructionSetBuilder()
+    with builder() as Instruction:
+        Instruction(opname='LOAD_NAME', arg=0, argval='x', starts_line=1)
+        Instruction(opname='JUMP_IF_FALSE_OR_POP', arg=label('else'))
+        Instruction(opname='LOAD_NAME', arg=1, argval='a')
+        Instruction(label='else',
+                    opname='JUMP_IF_TRUE_OR_POP', arg=label('out'))
+        Instruction(opname='LOAD_NAME', arg=2, argval='y')
+        Instruction(label='out',
+                    opname='RETURN_VALUE')
+    code = builder.code
+    if _py3:
+        code = types.CodeType(0, 0, 0, 3, 0, code, (), ('x', 'a', 'y'),
+                              (), '', '<module>', 1, b'')
+    else:
+        code = types.CodeType(0, 0, 3, 0, code, (), ('x', 'a', 'y'),
+                              (), '', '<module>', 1, '')
+    u = Uncompyled(code)
+    assert u.ast
+    # assert u.source == 'return x and a or y'
+
+
 def test_comprehensions():
     expressions = [
+        ('(x for x in this)', None),
+        ('(x for x in this if p(x))', None),
+
+        ('[x for x in this]', None),
+        ('[x for x in this if p(x)]', None),
+
         ('((x, y) for x, y in this)', None),
+        ('[(x, y) for x, y in this]', None),
+
         ('((a for a in b) for b in (x for x in this))', None),
         ('[[a for a in b] for b in [x for x in this]]', None),
         ('calling(a for a in this if a < y)', None),
@@ -108,7 +386,8 @@ def _do_test(expressions):
         u = None
         try:
             u = Uncompyled(code)
-            assert u.source == expected
+            assert u.ast
+            # assert u.source == expected
         except:
             print()
             print(expr)
