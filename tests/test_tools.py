c5bb5999bc74c0206d7338ab8de7f68f6b66ec84
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-11-17 17:00:26 -0500

    Add the tools module.

---
+++ tests/test_tools.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# test_tools
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-11-17
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+from xotl.ql.tools import detect_names
+
+
+def test_freenames_detection_1():
+    result = detect_names('all(x for x in this for y in those if p(y)'
+                          '      for z in y)')
+    expected = {'this', 'those', 'p', 'all'}
+    assert result == expected
+
+
+def test_freenames_detection_2():
+    result = detect_names('(lambda x: lambda p, y=p(x): x + y)(x)')
+    expected = {'p', 'x'}
+    assert result == expected
