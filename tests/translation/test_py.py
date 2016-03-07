2ec162e3565653ef254d1400fd3015ef1b6297e0
Manuel Vázquez Acosta <mva.led@gmail.com>
2016-03-07 09:07:49 -0500

    Extract fixtures to a model.

--- tests/translation/test_py.py
+++ tests/translation/test_py.py
@@ -15,68 +15,13 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_imports)
 
-from .metamodel import get_birth_date
-from .model import Person, Place, Entity
 
 from xotl.ql.core import this
 from xotl.ql.core import get_query_object
 from xotl.ql.translation.py import _TestPlan as translate
 
-
-# TODO:  Make this fixtures
-cuba = Place(name='Cuba', type='Country')
-havana = Place(name='Havana', type='Province', located_in=cuba)
-lisa = Place(name='La lisa', type='Municipality', located_in=havana)
-cotorro = Place(name='Cotorro', type='Municipality', located_in=havana)
-ciego = Place(name='Ciego de Ávila', type='Province', located_in=cuba)
-moron = Place(name='Morón', type='Municipality', located_in=ciego)
-
-
-elsa = Person(name='Elsa Acosta Cabrera',
-              birthdate=get_birth_date(65),
-              lives_in=moron)
-
-papi = Person(name='Manuel Vázquez Portal',
-              birthdate=get_birth_date(63))
-
-manu = Person(name='Manuel Vázquez Acosta',
-              birthdate=get_birth_date(34),
-              mother=elsa,
-              father=papi,
-              lives_in=lisa)
-
-denia = Person(name='Ana Denia Pérez',
-               birthdate=get_birth_date(58),
-               lives_in=cotorro)
-
-pedro = Person(name='Pedro Piñero',
-               birthdate=get_birth_date(60),
-               lives_in=cotorro)
-
-yade = Person(name='Yadenis Piñero Pérez',
-              birthdate=get_birth_date(33),
-              mother=denia,
-              father=pedro, lives_in=lisa)
-
-ppp = Person(name='Yobanis Piñero Pérez',
-             birthdate=get_birth_date(36),
-             mother=denia,
-             father=pedro,
-             lives_in=lisa)
-
-pedri = Person(name='Pedrito',
-               birthdate=get_birth_date(10),
-               father=ppp)
-
-carli = Person(name='Carli',
-               birthdate=get_birth_date(8),
-               father=ppp)
-
-manolito = Person(name='Manuel Vázquez Piñero',
-                  birthdate=get_birth_date(6),
-                  mother=yade,
-                  father=manu,
-                  lives_in=lisa)
+from .model import Person, Entity
+from .world import *    # noqa
 
 
 #
@@ -94,10 +39,6 @@ def test_nested_genexprs_with_thesefy():
     outer_uncomp = Uncompyled(outer)
     assert outer_uncomp.qst == expected_uncomp.qst
 
-    # Although semantically equiv, some sort of unimplemented transformation
-    # of the previous is needed.
-    # another = (p for p in this if isinstance(p, Person))
-
 
 def test_all_pred(**kwargs):
     plan1 = translate(
@@ -107,6 +48,8 @@ def test_all_pred(**kwargs):
          if parent.children),
         **kwargs
     )
+    import ipdb; ipdb.set_trace()    # TODO: Remove this
+
     result1 = set(plan1())
     assert elsa in result1
     assert papi in result1
---
+++ tests/translation/world.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# world
+# ---------------------------------------------------------------------
+# Copyright (c) 2016 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2016-03-07
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from .metamodel import get_birth_date
+from .model import Person, Place
+
+__all__ = [
+    'cuba', 'havana', 'lisa', 'cotorro', 'ciego', 'moron',
+    'elsa', 'papi', 'manu', 'denia', 'pedro', 'yade', 'ppp',
+    'pedri', 'carli', 'manolito'
+]
+
+
+# TODO:  Make this fixtures
+cuba = Place(name='Cuba', type='Country')
+havana = Place(name='Havana', type='Province', located_in=cuba)
+lisa = Place(name='La lisa', type='Municipality', located_in=havana)
+cotorro = Place(name='Cotorro', type='Municipality', located_in=havana)
+ciego = Place(name='Ciego de Ávila', type='Province', located_in=cuba)
+moron = Place(name='Morón', type='Municipality', located_in=ciego)
+
+
+elsa = Person(name='Elsa Acosta Cabrera',
+              birthdate=get_birth_date(65),
+              lives_in=moron)
+
+papi = Person(name='Manuel Vázquez Portal',
+              birthdate=get_birth_date(63))
+
+manu = Person(name='Manuel Vázquez Acosta',
+              birthdate=get_birth_date(34),
+              mother=elsa,
+              father=papi,
+              lives_in=lisa)
+
+denia = Person(name='Ana Denia Pérez',
+               birthdate=get_birth_date(58),
+               lives_in=cotorro)
+
+pedro = Person(name='Pedro Piñero',
+               birthdate=get_birth_date(60),
+               lives_in=cotorro)
+
+yade = Person(name='Yadenis Piñero Pérez',
+              birthdate=get_birth_date(33),
+              mother=denia,
+              father=pedro, lives_in=lisa)
+
+ppp = Person(name='Yobanis Piñero Pérez',
+             birthdate=get_birth_date(36),
+             mother=denia,
+             father=pedro,
+             lives_in=lisa)
+
+pedri = Person(name='Pedrito',
+               birthdate=get_birth_date(10),
+               father=ppp)
+
+carli = Person(name='Carli',
+               birthdate=get_birth_date(8),
+               father=ppp)
+
+manolito = Person(name='Manuel Vázquez Piñero',
+                  birthdate=get_birth_date(6),
+                  mother=yade,
+                  father=manu,
+                  lives_in=lisa)
