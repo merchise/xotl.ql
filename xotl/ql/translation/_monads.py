473285fd419246c2665f7c7ed6abb9912440ed9e
Manuel Vázquez Acosta <mva.led@gmail.com>
2016-07-04 16:53:10 -0400

    Extract Infinity.

--- xotl/ql/translation/_monads.py
+++ xotl/ql/translation/_monads.py
@@ -26,10 +26,11 @@ from __future__ import (division as _py3_division,
                         absolute_import as _py3_abs_import)
 
 import operator
-from functools import total_ordering
 
 from xoutil import Undefined
 from xotl.ql import qst
+
+from ._infinity import Infinity
 
 import sys
 _py_version = sys.version_info
@@ -530,57 +531,6 @@ class SortedCons(Type):
                 return Cons(x, Cons(y, ys)())
             else:
                 return Cons(y, SortedCons(self.order, x, ys)())
-
-
-@total_ordering
-class InfinityType(object):
-    _positive = None
-    _negative = None
-
-    def __new__(cls, sign):
-        if sign < 0:
-            res = cls._negative
-            if not res:
-                cls._negative = res = object.__new__(cls)
-                res.sign = -1
-        else:
-            res = cls._positive
-            if not res:
-                cls._positive = res = object.__new__(cls)
-                res.sign = 1
-        return res
-
-    def __init__(self, sign):
-        self.sign = -1 if sign < 0 else 1
-
-    def __lt__(self, other):
-        from numbers import Number
-        if isinstance(other, Number):
-            return self.sign < 0   # True iff -Infinity
-        elif isinstance(other, InfinityType):
-            return self.sign < other.sign
-        else:
-            raise TypeError('Incomparable types')
-
-    def __eq__(self, other):
-        from numbers import Number
-        if isinstance(other, Number):
-            return False
-        elif isinstance(other, InfinityType):
-            return self.sign == other.sign
-        else:
-            raise TypeError('Incomparable types')
-
-    def __neg__(self):
-        return type(self)(-self.sign)
-
-    def __str__(self):
-        return '∞' if self.sign > 0 else '-∞'
-
-    def __repr__(self):
-        return 'Infinity' if self.sign > 0 else '-Infinity'
-
-Infinity = InfinityType(+1)
 
 Min = Foldr(lambda x, y: x if x < y else y, Infinity)
 Max = Foldr(lambda x, y: x if x > y else y, -Infinity)
